06d4b7d523cb337cf24b3b65e0614de3033f6620
Seth <sjw3kf@mail.missouri.edu>
2014-12-05 17:01:35 -0600

    Added documentation

--- src/main/java/ActorPaths.java
+++ src/main/java/ActorPaths.java
@@ -21,6 +21,17 @@ public class ActorPaths {
 
     public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 
+        /**
+         * Maps each path of tab delimited values to its first and last elements
+         * Example:
+         * value = "a b c"
+         * mapping = "a" -> "a b c" and "c" -> "a b c"
+         * @param key The line number of the value
+         * @param value A path of tab delimited actors
+         * @param context The ouput
+         * @throws IOException
+         * @throws InterruptedException
+         */
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
             final String trimmedValue = value.toString().trim();
@@ -42,11 +53,19 @@ public class ActorPaths {
 
     public static class Reduce extends Reducer<Text, Text, Text, NullWritable> {
 
+        /**
+         * @param list A list of strings
+         * @return true if the list contains duplicates, false otherwise
+         */
         public boolean duplicates(List<String> list) {
             Set<String> set = new HashSet<String>(list);
             return !(set.size() == list.size());
         }
 
+        /**
+         * @param list A list of strings
+         * @return The values of the string combined, tab delimited
+         */
         public String buildString(List<String> list) {
 
             final StringBuilder builder = new StringBuilder(list.get(0));
@@ -58,6 +77,9 @@ public class ActorPaths {
             return builder.toString();
         }
 
+        /**
+         * A simple class to hold a pair of list of strings
+         */
         public static class Pair {
             public final List<String> head;
             public final List<String> tail;
@@ -68,6 +90,13 @@ public class ActorPaths {
             }
         }
 
+        /**
+         * Partition iterates over the iterable collection and separates values
+         * based on if the key exists at the beginning or end of string
+         * @param key Some key that either exists at the beginning or end of each string
+         * @param iterable An object that contains an iterator over a collection of Text objects
+         * @return A pair of the partitioned lists
+         */
         public Pair partition(String key, Iterable<Text> iterable) {
 
             final Iterator<Text> it = iterable.iterator();
@@ -88,10 +117,13 @@ public class ActorPaths {
 
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
+            //Partition each of the values depending on if the key exists in its head or tail
             final Pair pair = partition(key.toString(), values);
 
             for (String back : pair.head) {
 
+                //Remove the key from the values
+                //We will use the key in the corresponding array
                 final List<String> backActors = new ArrayList<String>(Arrays.asList(back.split("\t")));
                 backActors.remove(0);
 
@@ -99,13 +131,17 @@ public class ActorPaths {
 
                     final List<String> frontActors = new ArrayList<String>(Arrays.asList(front.split("\t")));
 
+                    //Check that the path that will be created is of the appropriate size
                     if (frontActors.size() + backActors.size() != pathLength) {
                         continue;
                     }
 
+                    //Combine the two lists
                     final List<String> path = new ArrayList<String>(frontActors);
                     path.addAll(backActors);
 
+                    //Skip this list if it contains duplicates
+                    //ex "c b c"
                     if (duplicates(path)) {
                         continue;
                     }
@@ -126,6 +162,7 @@ public class ActorPaths {
             System.err.println("Usage: <path length> <in dir> <out dir>");
         }
 
+        //Determine the length of the path to be created
         pathLength = Integer.parseInt(args[0]);
 
         Configuration conf = new Configuration();
